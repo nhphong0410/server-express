5b4ad9587bdef34f5d41fcd8eca240cb0cbcb6af
phong0410 <nguyenhongphong281099@gmail.com>
2024-03-12 17:42:41 +0700

    blah blah blah

--- src/services/hello-world.ts
+++ src/services/hello-world.ts
@@ -1,32 +1,19 @@
-import { configDotenv } from 'dotenv';
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
-configDotenv();
+import client from '../repos';
 
 class HelloWorldService {
   private client;
 
   constructor() {
-    this.client = new MongoClient(process.env.MONGODB_URI as string, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-      }
-    });
+    this.client = client;
   }
 
   async hello() {
     try {
-      await this.client.connect();
-
       const result = await this.client.db('Test').collection('Text').findOne();
 
       return result?.Text;
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.connect();
     }
   }
 };
